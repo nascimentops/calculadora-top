d4d05229745bcc2ff432db4a9443869797e13414
Caio <209200680+nascimentops@users.noreply.github.com>
2026-02-17 02:18:35 -0300

    add: operações com a calculadora

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
+const operators = document.querySelectorAll(".operator");
 const clear = document.querySelector("#clear");
+const equal = document.querySelector("#equal")
+
+let number1 = "";
+let number2 = "";
+let operator = "";
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (e) => {
-        display.textContent += e.target.textContent;
+        number1 += e.target.textContent;
+        display.textContent = number1;
     });
 };
 
 clear.addEventListener("click", () => {
+    number1 = "";
+    number2 = "";
     display.textContent = "";
+    operator = "";
 });
+
+equal.addEventListener("click", () => {
+    if (!number1 || !number2 || !operator) return;
+    number2 = String(operate(operator, Number(number2), Number(number1)));
+    number1 = "";
+    if (number2.length >= 15 && number2.split("").includes(".")){
+        display.textContent = Number(number2).toFixed(5);
+    } else {
+    display.textContent = number2;
+    }
+    number2 = "";
+})
+
+for (let i = 0; i < operators.length; i++){
+    operators[i].addEventListener("click", (e) => {
+        if (!number2 && number1){
+            number2 = number1;
+            number1 = "";
+            display.textContent = number1;
+            operator = e.target.textContent;
+        } else if (number1) {
+            number2 = String(operate(operator, Number(number2), Number(number1)));
+            number1 = "";
+            if (number2.length >= 15 && number2.split("").includes(".")){
+                display.textContent = Number(number2).toFixed(5);
+            } else {
+                display.textContent = number2;
+            }
+            operator = e.target.textContent;
+        } else {
+            operator = e.target.textContent;
+        }
+    })
+}
 
 function add (a, b) {
     return a + b;
@@ -25,6 +69,9 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
+    if (b === 0){
+        return "ERROR";
+    }
     return a / b;
 };
 
